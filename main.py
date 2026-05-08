48ec86eb40324d9472563f81f3fe7ffa88a87cc8
yaqeenbanisaker06-lgtm <yaqeenbanisaker06@gmail.com>
2026-05-08 15:17:20 +0300

    Initial EDA setup

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 #Full Exploratory Data Analysis (EDA)
-import pandas as pd 
-df = pd.read_csv(r"data/raw/OnlineNewsPopularity/OnlineNewsPopularity.csv")
-print(df.head())
-print(df.shape)
+import pandas as pd
+import numpy as np
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+sns.set_style('whitegrid')
+df=pd.read_csv("data/raw/OnlineNewsPopularity/OnlineNewsPopularity.csv")
+
+print("Data Shape",df.shape)
+df.head()
